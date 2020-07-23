2bf158391a74edc00b69397944c3dcafdc9fd2ea
AhmedA998 <62741306+AhmedA998@users.noreply.github.com>
2020-07-23 17:16:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,10 @@ def predict():
     '''
     int_features = [float(x) for x in request.form.values()]
     final_features = np.array(int_features)
-    prediction = model.predict([[final_features]])
-    predictionsProb =model.predict_proba([[final_features]])
-   # prediction = model.predict(np.array([[1,85,66,29,0,26.6,0.351,31]]))
-   # predictionsProb = model.predict_proba(np.array([[1,85,66,29,0,26.6,0.351,31]]))
+    #prediction = model.predict([[final_features]])
+    #predictionsProb =model.predict_proba([[final_features]])
+    prediction = model.predict(np.array([[1,85,66,29,0,26.6,0.351,31]]))
+    predictionsProb = model.predict_proba(np.array([[1,85,66,29,0,26.6,0.351,31]]))
     
     s=round(prediction[0,0])
     
@@ -36,18 +36,6 @@ def predict():
 
   # output = round(prediction[0], 2)
    
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    
-    data = request.get_json(force=True)
-    prediction = model.predict([[np.array(list(data.values()))]])
-
-    output = prediction[0]
-    return jsonify(output)
-
 
 
 if __name__ == "__main__":
