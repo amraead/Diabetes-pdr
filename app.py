33f29104022790f91d4b9f347a0e01a738228364
AhmedA998 <62741306+AhmedA998@users.noreply.github.com>
2020-07-23 17:23:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,18 @@ def predict():
 
   # output = round(prediction[0], 2)
    
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    
+    data = request.get_json(force=True)
+    prediction = model.predict([[np.array(list(data.values()))]])
+
+    output = prediction[0]
+    return jsonify(output)
+
 
 
 if __name__ == "__main__":
