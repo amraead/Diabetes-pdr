0a39d8aff0990a4cd36b581a8b620a01d6dfb16a
AhmedA998 <62741306+AhmedA998@users.noreply.github.com>
2020-07-23 17:47:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
 
 
 @app.route('/')
@@ -14,9 +13,8 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
+    global model
+    model = pickle.load(open('model.pkl', 'rb'))
     #int_features = [float(x) for x in request.form.values()]
     #final_features = np.array(int_features)
     #prediction = model.predict([[final_features]])
